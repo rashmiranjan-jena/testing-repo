00c5073e840e25cf1f05fb4219fddbd4e05144c4
rashmiranjan-jena <rashmirj1234@gmail.com>
2025-11-16 21:20:50 +0530

    add a new console by rashmi

--- server.js
+++ server.js
@@ -34,4 +34,5 @@ app.get("/", async (req, res) => {
 app.listen(PORT, () => {
   console.log(`🚀 Server is running on port ${PORT}`);
   console.log(`Visit http://localhost:${PORT}`);
+  console.log("Press Ctrl+C to quit.");
 });
